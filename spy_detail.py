3b36dcec5b0b1bc9345dd8cabd9ac305b89c1ca3
tanyasood <37610596+tanyasood@users.noreply.github.com>
2018-03-26 20:34:40 +0530

    Update spy_detail.py

--- spy_detail.py
+++ spy_detail.py
@@ -1,6 +1,6 @@
 print("SPYCHAT")
 print("Welcome to SPYCHAT")
-spy_name=input("What is your name")
+spy_name=input('What/s your name')
 print(spy_name)
 print("Welcome " + spy_name + " How are you?")
 type(spy_name)
@@ -9,7 +9,7 @@ print(spy_salutation)
 print(spy_salutation +" " + spy_name)
 print("Alright "+ spy_salutation +" " + spy_name + " I would like to know a little bit more about you")
 spy_account=input("Tell which account would you prefer to choose(default/new)")
-if(spy_account=="new"):      #if spy wants to create a new account ask spy to enter its details
+if(spy_account=="new"):
     spy_name = str(input("Whats your name"))
     print(spy_name)
     print("Welcome " + spy_name + " How are you?")
@@ -22,8 +22,8 @@ if(spy_account=="new"):      #if spy wants to create a new account ask spy to en
     spy_rating = 0.0
     spy_is_online = True
 spy_age = int(input("What is your age?"))
-if spy_age > 17 and spy_age < 60:         #if user age is b/w 17 and 16 ask the user to input its rating
-    spy_rating = float(input("What is your spy rating?"))    #float is used because rating has decimal value
+if spy_age > 17 and spy_age < 60:
+    spy_rating = float(input("What is your spy rating?"))
     if spy_rating > 4.5:
         print("Great ace!")
     elif spy_rating > 3.5 and spy_rating <= 4.5:
@@ -34,13 +34,9 @@ if spy_age > 17 and spy_age < 60:         #if user age is b/w 17 and 16 ask the 
         print("We can always use somebody to help in the office.")
 
 else:
-    print ("sorry you are not of correct age to spy")
-print('Welcome in python class ' +  spy_salutation + " " + spy_name )
-print(spy_age )
-print(spy_rating )
-print("Thank u")
+    exit(0)
 
-
+print('welcome in python class' + spy_salutation + " " + spy_name + " \n age :"+ str(spy_age) + " \n rating:"  + str(spy_rating) + "\n proud to have you onboard")
 
 
 
